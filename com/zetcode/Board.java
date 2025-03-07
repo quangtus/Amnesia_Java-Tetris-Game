8bd279d9fbcb152472bc11e60a30d941ccf21a37
quangtus <stu725105183@hnue.edu.vn>
2025-03-07 08:01:33 +0700

    them nhieu chuc nang

--- com/zetcode/Board.java
+++ com/zetcode/Board.java
@@ -3,6 +3,7 @@ package com.zetcode;
 import com.zetcode.Shape.Tetrominoe;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 import java.awt.Color;
@@ -14,9 +15,13 @@ import java.awt.event.KeyEvent;
 
 public class Board extends JPanel {
 
+    private boolean showGhostPiece = false; // Biến để kiểm soát hiển thị khối bóng ma
+    private String username;
+    private boolean isDarkMode = false;
+
     private final int BOARD_WIDTH = 10;
     private final int BOARD_HEIGHT = 22;
-    private final int PERIOD_INTERVAL = 300;
+    private final int PERIOD_INTERVAL = 100;
 
     private Timer timer;
     private boolean isFallingFinished = false;
@@ -28,214 +33,175 @@ public class Board extends JPanel {
     private Shape curPiece;
     private Tetrominoe[] board;
 
-    public Board(Tetris parent) {
-
+    public Board(Tetris parent, String username) {
+        this.username = username;
         initBoard(parent);
     }
 
     private void initBoard(Tetris parent) {
-
         setFocusable(true);
         statusbar = parent.getStatusBar();
         addKeyListener(new TAdapter());
     }
 
     private int squareWidth() {
-
         return (int) getSize().getWidth() / BOARD_WIDTH;
     }
 
     private int squareHeight() {
-
         return (int) getSize().getHeight() / BOARD_HEIGHT;
     }
 
     private Tetrominoe shapeAt(int x, int y) {
-
         return board[(y * BOARD_WIDTH) + x];
     }
 
     void start() {
-
         curPiece = new Shape();
         board = new Tetrominoe[BOARD_WIDTH * BOARD_HEIGHT];
-
         clearBoard();
         newPiece();
-
         timer = new Timer(PERIOD_INTERVAL, new GameCycle());
         timer.start();
     }
 
     private void pause() {
-
         isPaused = !isPaused;
-
         if (isPaused) {
-
+            timer.stop();
             statusbar.setText("paused");
         } else {
-
+            timer.start();
             statusbar.setText(String.valueOf(numLinesRemoved));
         }
-
         repaint();
     }
 
     @Override
     public void paintComponent(Graphics g) {
-
         super.paintComponent(g);
         doDrawing(g);
+        
+        if (showGhostPiece) {
+            drawGhostPiece(g);
+        }
     }
 
     private void doDrawing(Graphics g) {
-
         var size = getSize();
         int boardTop = (int) size.getHeight() - BOARD_HEIGHT * squareHeight();
 
+        if (isDarkMode) {
+            setBackground(Color.BLACK);
+        } else {
+            setBackground(Color.WHITE);
+        }
+
         for (int i = 0; i < BOARD_HEIGHT; i++) {
-
             for (int j = 0; j < BOARD_WIDTH; j++) {
-
                 Tetrominoe shape = shapeAt(j, BOARD_HEIGHT - i - 1);
-
                 if (shape != Tetrominoe.NoShape) {
-
-                    drawSquare(g, j * squareWidth(),
-                            boardTop + i * squareHeight(), shape);
+                    drawSquare(g, j * squareWidth(), boardTop + i * squareHeight(), shape);
                 }
             }
         }
 
         if (curPiece.getShape() != Tetrominoe.NoShape) {
-
             for (int i = 0; i < 4; i++) {
-
                 int x = curX + curPiece.x(i);
                 int y = curY - curPiece.y(i);
-
-                drawSquare(g, x * squareWidth(),
-                        boardTop + (BOARD_HEIGHT - y - 1) * squareHeight(),
-                        curPiece.getShape());
+                drawSquare(g, x * squareWidth(), boardTop + (BOARD_HEIGHT - y - 1) * squareHeight(), curPiece.getShape());
             }
         }
     }
 
     private void dropDown() {
-
         int newY = curY;
-
         while (newY > 0) {
-
             if (!tryMove(curPiece, curX, newY - 1)) {
-
                 break;
             }
-
             newY--;
         }
-
         pieceDropped();
     }
 
     private void oneLineDown() {
-
         if (!tryMove(curPiece, curX, curY - 1)) {
-
             pieceDropped();
         }
     }
 
     private void clearBoard() {
-
         for (int i = 0; i < BOARD_HEIGHT * BOARD_WIDTH; i++) {
-
             board[i] = Tetrominoe.NoShape;
         }
     }
 
     private void pieceDropped() {
-
         for (int i = 0; i < 4; i++) {
-
             int x = curX + curPiece.x(i);
             int y = curY - curPiece.y(i);
             board[(y * BOARD_WIDTH) + x] = curPiece.getShape();
         }
-
         removeFullLines();
-
         if (!isFallingFinished) {
-
             newPiece();
         }
     }
 
     private void newPiece() {
-
         curPiece.setRandomShape();
         curX = BOARD_WIDTH / 2 + 1;
         curY = BOARD_HEIGHT - 1 + curPiece.minY();
-
         if (!tryMove(curPiece, curX, curY)) {
-
             curPiece.setShape(Tetrominoe.NoShape);
             timer.stop();
-
-            var msg = String.format("Game over. Score: %d", numLinesRemoved);
-            statusbar.setText(msg);
+            int highScore = Database.getHighScore(username);
+            String msg = String.format("Game over. Score: %d. High score: %d", numLinesRemoved, highScore);
+            if (numLinesRemoved > highScore) {
+                Database.updateHighScore(username, numLinesRemoved);
+                msg += "\nChúc mừng bạn đã phá kỷ lục!";
+            }
+            int option = JOptionPane.showConfirmDialog(this, msg + "\nBạn có muốn chơi lại không?", "Game Over", JOptionPane.YES_NO_OPTION);
+            if (option == JOptionPane.YES_OPTION) {
+                restartGame();
+            } else {
+                statusbar.setText(msg);
+            }
         }
     }
 
     private boolean tryMove(Shape newPiece, int newX, int newY) {
-
         for (int i = 0; i < 4; i++) {
-
             int x = newX + newPiece.x(i);
             int y = newY - newPiece.y(i);
-
             if (x < 0 || x >= BOARD_WIDTH || y < 0 || y >= BOARD_HEIGHT) {
-
                 return false;
             }
-
             if (shapeAt(x, y) != Tetrominoe.NoShape) {
-
                 return false;
             }
         }
-
         curPiece = newPiece;
         curX = newX;
         curY = newY;
-
         repaint();
-
         return true;
     }
 
     private void removeFullLines() {
-
         int numFullLines = 0;
-
         for (int i = BOARD_HEIGHT - 1; i >= 0; i--) {
-
             boolean lineIsFull = true;
-
             for (int j = 0; j < BOARD_WIDTH; j++) {
-
                 if (shapeAt(j, i) == Tetrominoe.NoShape) {
-
                     lineIsFull = false;
                     break;
                 }
             }
-
             if (lineIsFull) {
-
                 numFullLines++;
-
                 for (int k = i; k < BOARD_HEIGHT - 1; k++) {
                     for (int j = 0; j < BOARD_WIDTH; j++) {
                         board[(k * BOARD_WIDTH) + j] = shapeAt(j, k + 1);
@@ -243,11 +209,8 @@ public class Board extends JPanel {
                 }
             }
         }
-
         if (numFullLines > 0) {
-
             numLinesRemoved += numFullLines;
-
             statusbar.setText(String.valueOf(numLinesRemoved));
             isFallingFinished = true;
             curPiece.setShape(Tetrominoe.NoShape);
@@ -255,22 +218,20 @@ public class Board extends JPanel {
     }
 
     private void drawSquare(Graphics g, int x, int y, Tetrominoe shape) {
-
         Color colors[] = {new Color(0, 0, 0), new Color(204, 102, 102),
                 new Color(102, 204, 102), new Color(102, 102, 204),
                 new Color(204, 204, 102), new Color(204, 102, 204),
                 new Color(102, 204, 204), new Color(218, 170, 0)
         };
-
         var color = colors[shape.ordinal()];
-
+        if (isDarkMode) {
+            color = color.darker();
+        }
         g.setColor(color);
         g.fillRect(x + 1, y + 1, squareWidth() - 2, squareHeight() - 2);
-
         g.setColor(color.brighter());
         g.drawLine(x, y + squareHeight() - 1, x, y);
         g.drawLine(x, y, x + squareWidth() - 1, y);
-
         g.setColor(color.darker());
         g.drawLine(x + 1, y + squareHeight() - 1,
                 x + squareWidth() - 1, y + squareHeight() - 1);
@@ -279,52 +240,119 @@ public class Board extends JPanel {
     }
 
     private class GameCycle implements ActionListener {
-
         @Override
         public void actionPerformed(ActionEvent e) {
-
             doGameCycle();
         }
     }
 
     private void doGameCycle() {
-
         update();
         repaint();
     }
 
     private void update() {
-
         if (isPaused) {
-
             return;
         }
-
         if (isFallingFinished) {
-
             isFallingFinished = false;
             newPiece();
         } else {
-
             oneLineDown();
         }
     }
 
-    class TAdapter extends KeyAdapter {
+    private void restartGame() {
+        numLinesRemoved = 0;
+        isFallingFinished = false;
+        clearBoard();
+        newPiece();
+        timer.start();
+        statusbar.setText("0");
+    }
 
+    private void confirmRestart() {
+        pause(); // Dừng trò chơi
+        int option = JOptionPane.showConfirmDialog(this, "Bạn có muốn chơi lại không?", "Restart Game", JOptionPane.YES_NO_OPTION);
+        if (option == JOptionPane.YES_OPTION) {
+            restartGame();
+        } else {
+            pause(); // Tiếp tục trò chơi nếu người dùng chọn "No"
+        }
+    }
+    private void toggleGhostPiece() {
+        showGhostPiece = !showGhostPiece;
+        repaint();
+    }
+
+    private int findDropPosition() {
+        int newY = curY;
+        while (newY > 0) {
+            boolean canMove = true;
+            
+            for (int i = 0; i < 4; i++) {
+                int x = curX + curPiece.x(i);
+                int y = (newY - 1) - curPiece.y(i);
+                
+                if (y < 0 || x < 0 || x >= BOARD_WIDTH || 
+                    shapeAt(x, y) != Tetrominoe.NoShape) {
+                    canMove = false;
+                    break;
+                }
+            }
+            
+            if (!canMove) {
+                break;
+            }
+            
+            newY--;
+        }
+        return newY;
+    }
+    private void drawGhostPiece(Graphics g) {
+        if (curPiece.getShape() == Tetrominoe.NoShape) {
+            return;
+        }
+        
+        int ghostY = findDropPosition();
+        
+        var size = getSize();
+        int boardTop = (int) size.getHeight() - BOARD_HEIGHT * squareHeight();
+        
+        for (int i = 0; i < 4; i++) {
+            int x = curX + curPiece.x(i);
+            int y = ghostY - curPiece.y(i);
+            drawGhostSquare(g, x * squareWidth(), boardTop + (BOARD_HEIGHT - y - 1) * squareHeight());
+        }
+    }
+    
+    private void drawGhostSquare(Graphics g, int x, int y) {
+        // Giảm alpha từ 128 xuống 70 để tăng độ trong suốt
+        g.setColor(new Color(128, 128, 128, 70)); // Màu xám với độ trong suốt cao hơn
+        g.fillRect(x + 1, y + 1, squareWidth() - 2, squareHeight() - 2);
+        g.setColor(new Color(192, 192, 192, 70)); // Màu xám sáng với độ trong suốt cao hơn
+        g.drawLine(x, y + squareHeight() - 1, x, y);
+        g.drawLine(x, y, x + squareWidth() - 1, y);
+        g.setColor(new Color(64, 64, 64, 70)); // Màu xám tối với độ trong suốt cao hơn
+        g.drawLine(x + 1, y + squareHeight() - 1,
+                x + squareWidth() - 1, y + squareHeight() - 1);
+        g.drawLine(x + squareWidth() - 1, y + squareHeight() - 1,
+                x + squareWidth() - 1, y + 1);
+    }
+    private void toggleDarkMode() {
+        isDarkMode = !isDarkMode;
+        repaint();
+    }
+
+    class TAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
-
             if (curPiece.getShape() == Tetrominoe.NoShape) {
-
                 return;
             }
-
             int keycode = e.getKeyCode();
-
-            // Java 12 switch expressions
             switch (keycode) {
-
                 case KeyEvent.VK_P -> pause();
                 case KeyEvent.VK_LEFT -> tryMove(curPiece, curX - 1, curY);
                 case KeyEvent.VK_RIGHT -> tryMove(curPiece, curX + 1, curY);
@@ -332,6 +360,9 @@ public class Board extends JPanel {
                 case KeyEvent.VK_UP -> tryMove(curPiece.rotateLeft(), curX, curY);
                 case KeyEvent.VK_SPACE -> dropDown();
                 case KeyEvent.VK_D -> oneLineDown();
+                case KeyEvent.VK_L -> toggleDarkMode();
+                case KeyEvent.VK_R -> confirmRestart(); // Thêm dòng này
+                case KeyEvent.VK_G -> toggleGhostPiece(); 
             }
         }
     }
--- com/zetcode/Tetris.java
+++ com/zetcode/Tetris.java
@@ -1,9 +1,11 @@
 package com.zetcode;
 
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 /*
 Java Tetris game clone
@@ -14,38 +16,35 @@ Website: https://zetcode.com
 public class Tetris extends JFrame {
 
     private JLabel statusbar;
+    private String username;
 
-    public Tetris() {
-
+    public Tetris(String username) {
+        this.username = username;
         initUI();
     }
 
     private void initUI() {
-
         statusbar = new JLabel(" 0");
         add(statusbar, BorderLayout.SOUTH);
 
-        var board = new Board(this);
+        var board = new Board(this, username);
         add(board);
         board.start();
 
         setTitle("Tetris");
-        setSize(200, 400);
+        setSize(400, 800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
     }
 
     JLabel getStatusBar() {
-
         return statusbar;
     }
 
     public static void main(String[] args) {
-
         EventQueue.invokeLater(() -> {
-
-            var game = new Tetris();
-            game.setVisible(true);
+            var loginFrame = new LoginFrame();
+            loginFrame.setVisible(true);
         });
     }
 }
